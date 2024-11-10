5f0340e8c5acdeb13709d61bfb4c0a72508600a7
jmwingenroth <jwing421@gmail.com>
2024-11-10 18:11:02 -0500

    Add electricity and thermal data

---
+++ lib/data_io.r
@@ -0,0 +1,41 @@
+### Read CSV files from data directory ###
+
+read_ggi_data <- function(datadir) {
+
+    paths <- list.files(datadir, ".csv", full.names = TRUE)
+    names <- list.files(datadir, ".csv", full.names = FALSE)
+    tidy_names <- sapply(names, tools::file_path_sans_ext)
+
+    output <- lapply(paths, read.csv)
+    names(output) <- tidy_names
+
+    return(output)
+
+}
+
+
+### Write secondary aluminum data ###
+
+write_2nd_alum <- function(datadir){
+
+    # input static aluminum 
+    second_aluminum_ggi <- data.frame(
+        Source = c(rep("Primary", 5), rep("Scrap", 5), rep("Primary", 5), rep("Scrap", 5)), 
+        Scrap_Amount = rep(c("0%", "25%", "50%", "75%", "100%"), 4),
+        Scrap_Amount_num = rep(c(0, 0.25, .50, .75, 1), 4), 
+        GGI_Amount = c(2.25, 1.68, 1.12, 0.56, 0, 0, 0.07, 0.13, 0.20, 0.26, 19.87, 14.90, 9.93, 4.97, 0.00, 0.00, 0.14, 0.28, 0.14, 0.56),
+        Emissions_Scenario = c(rep("Low", 10), rep("High", 10))
+    )
+
+    second_aluminum_ggi_cus <- data.frame(
+        Source = c(rep("Primary", 5), rep("Scrap", 5)),
+        Scrap_Amount = rep(c("0%", "25%", "50%", "75%", "100%"), 2),
+        Scrap_Amount_num = rep(c(0, 0.25, .50, .75, 1), 2),
+        Emissions_Scenario =c(rep("Custom", 10))
+    )
+
+    write.csv(second_aluminum_ggi, file.path(datadir, "second_aluminum_ggi.csv"), row.names = FALSE)
+    write.csv(second_aluminum_ggi_cus, file.path(datadir, "second_aluminum_ggi_cus.csv"), row.names = FALSE)
+
+}
+
